1557ebba4f325a312285dbc144b122f8c45a625c
Llydia Cross <llydiacross@gmail.com>
2024-04-14 19:01:30 +0100

    fix

--- dev/src/lib/stores/brower.store.ts
+++ dev/src/lib/stores/brower.store.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { Computer } from "backend/dist/";
+import { Computer } from "backend/src/generated/client";
 
 type State = {
   history: Record<string, ({ tab?: string; domain?: string } & Computer)[]>;
